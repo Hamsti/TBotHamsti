f90a680a9be60c85c4b8aba07addcb22a3536411
Hamsti <35877392+Hamsti@users.noreply.github.com>
2021-08-08 21:50:16 +0300

    Added the documentation for /Models/Messages/* and refactored TextMessage

--- HamstiBotWPF/Models/Messages/IMessage.cs
+++ HamstiBotWPF/Models/Messages/IMessage.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Windows;
 
 namespace TBotHamsti.Models.Messages
 {
+    /// <summary>
+    /// Common <see cref="IMessage"/> interface for logging
+    /// </summary>
     public interface IMessage
     {
+        /// <value>
+        /// <see cref="DateTime"/> when the log <see cref="IMessage"/> was created
+        /// </value>
         DateTime DateTimeGetMessage { get; }
+
+        /// <value>
+        /// <see cref="HorizontalAlignment"/> of the <see cref="IMessage"/> on the <see cref="System.Windows.Controls.Page"/>
+        /// </value>
+        HorizontalAlignment HorizontalAlignment { get; }
     }
 }
--- HamstiBotWPF/Models/Messages/TextMessage.cs
+++ HamstiBotWPF/Models/Messages/TextMessage.cs
@@ -3,21 +3,33 @@ using System.Windows;
 
 namespace TBotHamsti.Models.Messages
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class TextMessage : IMessage
     {
-        public string Text { get; private set; }
-        public DateTime DateTimeGetMessage { get; private set; }
-        public HorizontalAlignment HorizontalAlignment { get; set; } = HorizontalAlignment.Stretch;
+        /// <value>
+        /// Log text without date
+        /// </value>
+        public string Text { get; }
+        public DateTime DateTimeGetMessage { get; }
+        public HorizontalAlignment HorizontalAlignment { get; }
 
+        /// <summary>
+        /// <inheritdoc cref="TextMessage(string, HorizontalAlignment)" path="/summary"/> = <see cref="HorizontalAlignment.Stretch"/>
+        /// </summary>
+        /// <inheritdoc cref="TextMessage(string, HorizontalAlignment)" path="/param"/>
+        public TextMessage(string text) : this(text, HorizontalAlignment.Stretch) { }
 
-        public TextMessage(string text)
+        /// <summary>
+        /// Creating the new <see cref="IMessage"/> = <paramref name="text"/> type containing the current <see cref="DateTimeGetMessage"/> with <paramref name="horizontalAlignment"/>
+        /// </summary>
+        /// <param name="text"><inheritdoc cref="Text" path="/value"/></param>
+        /// <param name="horizontalAlignment"><inheritdoc cref="HorizontalAlignment" path="/value"/></param>
+        public TextMessage(string text, HorizontalAlignment horizontalAlignment)
         {
             Text = text;
             DateTimeGetMessage = DateTime.Now;
-        }
-
-        public TextMessage(string text, HorizontalAlignment horizontalAlignment) : this(text)
-        {
             HorizontalAlignment = horizontalAlignment;
         }
     }
